dadb26007e872e2a0991861c14a350596ca3f65a
RayhanAymanMangku <22523093@students.uii.ac.id>
2025-09-12 01:02:26 +0700

    feat: seo

--- src/app/(public)/craft/layout.tsx
+++ src/app/(public)/craft/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 export const metadata:Metadata = {
   title: "Kerajinan",
+  description: "Jelajahi beragam kerajinan lokal berkualitas dari Purbayan, mulai dari perhiasan perak hingga kerajinan tangan unik lainnya. Temukan keindahan dan keahlian pengrajin kami yang menciptakan karya seni yang memukau.",
 }
 
 export default function RootLayout({
