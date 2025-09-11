bd7724e41a7dcfcf5ca2a44861acb12279eeb726
RayhanAymanMangku <22523093@students.uii.ac.id>
2025-09-12 00:34:21 +0700

    feat: seo

---
+++ src/app/(public)/contact/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+export const metadata:Metadata = {
+  title: "Kontak",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <main>
+      {children}
+    </main>
+  );
+}
